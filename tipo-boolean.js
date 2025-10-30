5c41e46f54ccf72b362082617e9f6c02f28144ac
Thiago Fabri <thiagobarrosfabri07@gmail.com>
2025-10-30 12:22:51 -0300

    Add files via upload

---
+++ tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = 'Thiago';
+const estaAprovado = true;
+
+if (estaAprovado === true){
+    console.log('Parabéns, boas festas!');
+}else {
+    console.log('Reprovado, boas festas!');
+}
+
+if (estudante === 'Thiago'){
+    console.log(`Olá, ${estudante}`)
+}else {
+    console.log('Quem é vc?')
+}
